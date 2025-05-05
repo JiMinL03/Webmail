955fbec20f1c0c42eb61bb3c3a08ae05e0a189c7
kimeunbinbev <94042060+kimeunbinbev@users.noreply.github.com>
2025-05-05 15:53:23 +0900

    Sonarlint로 리팩토링

--- src/main/java/deu/cse/spring_webmail/model/MessageParser.java
+++ src/main/java/deu/cse/spring_webmail/model/MessageParser.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package deu.cse.spring_webmail.model;
 
 import deu.cse.spring_webmail.PropertyReader;
@@ -10,179 +6,135 @@ import jakarta.mail.Address;
 import jakarta.mail.Message;
 import jakarta.mail.Multipart;
 import jakarta.mail.Part;
-import jakarta.mail.internet.MimeUtility;
-import java.io.File;
-import java.io.FileOutputStream;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.mail.internet.MimeUtility;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
-/**
- *
- * @author skylo
- */
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 @Slf4j
 @RequiredArgsConstructor
 public class MessageParser {
 
-    @NonNull
-    @Getter
-    @Setter
-    private Message message;
-    @NonNull
-    @Getter
-    @Setter
-    private String userid;
-    @Getter
-    @Setter
-    private String toAddress;
-    @Getter
-    @Setter
-    private String fromAddress;
-    @Getter
-    @Setter
-    private String ccAddress;
-    @Getter
-    @Setter
-    private String sentDate;
-    @Getter
-    @Setter
-    private String subject;
-    @Getter
-    @Setter
-    private String body;
-    @Getter
-    @Setter
-    private String fileName;
-    @Getter
-    @Setter
-    private String downloadTempDir = "C:/temp/download/";
+    @NonNull @Getter @Setter private Message message;
+    @NonNull @Getter @Setter private String userid;
+
+    @Getter @Setter private String toAddress;
+    @Getter @Setter private String fromAddress;
+    @Getter @Setter private String ccAddress;
+    @Getter @Setter private String sentDate;
+    @Getter @Setter private String subject;
+    @Getter @Setter private String body;
+    @Getter @Setter private String fileName;
+    @Getter @Setter private String downloadTempDir = "C:/temp/download/";
 
     public MessageParser(Message message, String userid, HttpServletRequest request) {
-        this(message, userid);
+        this.message = message;
+        this.userid = userid;
+
         PropertyReader props = new PropertyReader();
         String downloadPath = props.getProperty("file.download_folder");
         downloadTempDir = request.getServletContext().getRealPath(downloadPath);
+
         File f = new File(downloadTempDir);
-        if (!f.exists()) {
-            f.mkdir();
+        if (!f.exists() && !f.mkdir()) {
+            log.warn("다운로드 디렉터리 생성 실패: {}", downloadTempDir);
         }
     }
 
     public boolean parse(boolean parseBody) {
-        boolean status = false;
-
         try {
-            getEnvelope(message);
+            extractEnvelope();
             if (parseBody) {
-                getPart(message);
+                processPart(message);
             }
-            // 220611 LJM: 필요시 true로 하여 메시지 본문 볼 수 있도록 할 것.
-            // printMessage(false);  
-            //  예외가 발생하지 않았으므로 정상적으로 동작하였음.
-            status = true;
+            return true;
         } catch (Exception ex) {
-            log.error("MessageParser.parse() - Exception : {}", ex.getMessage());
-            status = false;
-        } finally {
-            return status;
+            log.error("MessageParser.parse() - Exception : {}", ex.getMessage(), ex);
+            return false;
         }
     }
 
-    private void getEnvelope(Message m) throws Exception {
-        fromAddress = message.getFrom()[0].toString();  // 101122 LJM : replaces getMyFrom2()
+    // 메일 헤더 정보 추출
+    private void extractEnvelope() throws Exception {
+        fromAddress = message.getFrom()[0].toString();
         toAddress = getAddresses(message.getRecipients(Message.RecipientType.TO));
         Address[] addr = message.getRecipients(Message.RecipientType.CC);
-        if (addr != null) {
-            ccAddress = getAddresses(addr);
-        } else {
-            ccAddress = "";
-        }
+        ccAddress = (addr != null) ? getAddresses(addr) : "";
         subject = message.getSubject();
         sentDate = message.getSentDate().toString();
-        sentDate = sentDate.substring(0, sentDate.length() - 8);  // 8 for "KST 20XX"
-    }
-
-    // ref: http://www.oracle.com/technetwork/java/faq-135477.html#readattach
-    private void getPart(Part p) throws Exception {
-        String disp = p.getDisposition();
-
-        if (disp != null && (disp.equalsIgnoreCase(Part.ATTACHMENT)
-                || disp.equalsIgnoreCase(Part.INLINE))) {  // 첨부 파일
-//            fileName = p.getFileName();
-            fileName = MimeUtility.decodeText(p.getFileName());
-//            fileName = fileName.replaceAll(" ", "%20");
-            if (fileName != null) {
-                // 첨부 파일을 서버의 내려받기 임시 저장소에 저장
-                String tempUserDir = this.downloadTempDir + File.separator + this.userid;
-                File dir = new File(tempUserDir);
-                if (!dir.exists()) {  // tempUserDir 생성
-                    dir.mkdir();
-                }
-
-                String filename = MimeUtility.decodeText(p.getFileName());
-                // 파일명에 " "가 있을 경우 서블릿에 파라미터로 전달시 문제 발생함.
-                // " "를 모두 "_"로 대체함.
-//                filename = filename.replaceAll("%20", " ");
-                DataHandler dh = p.getDataHandler();
-                FileOutputStream fos = new FileOutputStream(tempUserDir + File.separator + filename);
-                dh.writeTo(fos);
-                fos.flush();
-                fos.close();
-            }
-        } else {  // 메일 본문
-            if (p.isMimeType("text/*")) {
-                body = (String) p.getContent();
-                if (p.isMimeType("text/plain")) {
-                    body = body.replaceAll("\r\n", " <br>");
-                }
-            } else if (p.isMimeType("multipart/alternative")) {
-                // html text보다  plain text 선호
-                Multipart mp = (Multipart) p.getContent();
-                for (int i = 0; i < mp.getCount(); i++) {
-                    Part bp = mp.getBodyPart(i);
-                    if (bp.isMimeType("text/plain")) {  // "text/html"도 있을 것임.
-                        getPart(bp);
-                    }
-                }
-            } else if (p.isMimeType("multipart/*")) {
-                Multipart mp = (Multipart) p.getContent();
-                for (int i = 0; i < mp.getCount(); i++) {
-                    getPart(mp.getBodyPart(i));
-                }
-            }
+        if (sentDate.length() > 8) {
+            sentDate = sentDate.substring(0, sentDate.length() - 8);
         }
     }
 
-    private void printMessage(boolean printBody) {
-        System.out.println("From: " + fromAddress);
-        System.out.println("To: " + toAddress);
-        System.out.println("CC: " + ccAddress);
-        System.out.println("Date: " + sentDate);
-        System.out.println("Subject: " + subject);
+    // 메일 본문 또는 첨부파일 처리
+    private void processPart(Part part) throws Exception {
+        String disposition = part.getDisposition();
 
-        if (printBody) {
-            System.out.println("본 문");
-            System.out.println("---------------------------------");
-            System.out.println(body);
-            System.out.println("---------------------------------");
-            System.out.println("첨부파일: " + fileName);
+        if (isAttachment(disposition)) {
+            handleAttachment(part);
+        } else if (part.isMimeType("text/*")) {
+            extractTextContent(part);
+        } else if (part.isMimeType("multipart/*")) {
+            processMultipart(part);
         }
     }
 
+    // 첨부파일 여부 확인
+    private boolean isAttachment(String disposition) {
+        return disposition != null &&
+                (disposition.equalsIgnoreCase(Part.ATTACHMENT) || disposition.equalsIgnoreCase(Part.INLINE));
+    }
+
+    // 첨부파일 저장 처리
+    private void handleAttachment(Part part) throws Exception {
+        fileName = MimeUtility.decodeText(part.getFileName());
+        if (fileName == null) return;
+
+        String tempUserDir = downloadTempDir + File.separator + userid;
+        File dir = new File(tempUserDir);
+        if (!dir.exists() && !dir.mkdir()) {
+            log.warn("사용자 디렉터리 생성 실패: {}", tempUserDir);
+        }
+
+        String safeFileName = fileName.replace(" ", "_");
+        try (FileOutputStream fos = new FileOutputStream(tempUserDir + File.separator + safeFileName)) {
+            part.getDataHandler().writeTo(fos);
+        }
+    }
+
+    // 텍스트 MIME 처리
+    private void extractTextContent(Part part) throws Exception {
+        body = (String) part.getContent();
+        if (part.isMimeType("text/plain")) {
+            body = body.replace("\r\n", " <br>");
+        }
+    }
+
+    // 멀티파트 처리 (재귀 호출)
+    private void processMultipart(Part part) throws Exception {
+        Multipart mp = (Multipart) part.getContent();
+        for (int i = 0; i < mp.getCount(); i++) {
+            processPart(mp.getBodyPart(i));
+        }
+    }
+
+    // 주소 배열을 문자열로 변환
     private String getAddresses(Address[] addresses) {
         StringBuilder buffer = new StringBuilder();
-
         for (Address address : addresses) {
-            buffer.append(address.toString());
-            buffer.append(", ");
-        } // 마지막에 있는 ", " 삭제
-        int start = buffer.length() - 2;
-        int end = buffer.length() - 1;
-        buffer.delete(start, end);
+            buffer.append(address.toString()).append(", ");
+        }
+        if (buffer.length() >= 2) {
+            buffer.setLength(buffer.length() - 2);
+        }
         return buffer.toString();
     }
 }
--- src/main/java/deu/cse/spring_webmail/model/SmtpAgent.java
+++ src/main/java/deu/cse/spring_webmail/model/SmtpAgent.java
@@ -1,10 +1,5 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package deu.cse.spring_webmail.model;
 
-import com.sun.mail.smtp.SMTPMessage;
 import jakarta.activation.DataHandler;
 import jakarta.activation.DataSource;
 import jakarta.activation.FileDataSource;
@@ -16,119 +11,118 @@ import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeBodyPart;
 import jakarta.mail.internet.MimeMultipart;
 import jakarta.mail.internet.MimeUtility;
+import jakarta.mail.internet.MimeMessage;
+
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Properties;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- *
- * @author jongmin
+ * SMTP 서버를 통해 메일을 전송하는 클래스
  */
 @Slf4j
 public class SmtpAgent {
 
-    @Getter @Setter  protected String host = null;
-    @Getter @Setter  protected String userid = null;
-    @Getter @Setter protected String to = null;
-    @Getter @Setter protected String cc = null;
-    @Getter @Setter protected String subj = null;
-    @Getter @Setter protected String body = null;
-    @Getter @Setter protected String file1 = null;
+    @Getter @Setter private String host;     // SMTP 서버 주소
+    @Getter @Setter private String userid;   // 송신자 이메일 주소
+    @Getter @Setter private String to;       // 수신자 이메일 주소
+    @Getter @Setter private String cc;       // 참조 이메일 주소
+    @Getter @Setter private String subj;     // 메일 제목
+    @Getter @Setter private String body;     // 메일 본문
+    @Getter @Setter private String file1;    // 첨부파일 경로
 
     public SmtpAgent(String host, String userid) {
         this.host = host;
         this.userid = userid;
     }
 
-    // LJM 100418 -  현재 로그인한 사용자의 이메일 주소를 반영하도록 수정되어야 함. - test only
-    // LJM 100419 - 일반 웹 서버와의 SMTP 동작시 setFrom() 함수 사용 필요함.
-    //              없을 경우 메일 전송이 송신주소가 없어서 걸러짐.
+    /**
+     * 메일을 구성하고 전송하는 메소드
+     */
     public boolean sendMessage() {
-        boolean status = false;
-
-        // 1. property 설정
         Properties props = System.getProperties();
         props.put("mail.debug", false);
         props.put("mail.smtp.host", this.host);
-        log.debug("SMTP host : {}", props.get("mail.smtp.host"));
+        log.debug("SMTP host: {}", props.get("mail.smtp.host"));
 
-        // 2. session 가져오기
         Session session = Session.getDefaultInstance(props, null);
         session.setDebug(false);
 
         try {
-            SMTPMessage msg = new SMTPMessage(session);
+            MimeMessage msg = new MimeMessage(session);
+            msg.setFrom(new InternetAddress(this.userid));  // 송신자 설정
 
-            // msg.setFrom(new InternetAddress(this.userid + "@" + this.host));
-            msg.setFrom(new InternetAddress(this.userid));  // 200102 LJM - 테스트 목적으로 수정
-            //msg.setFrom(new InternetAddress("jongmin@deu.ac.kr"));
-
-
-            // setRecipient() can be called repeatedly if ';' or ',' exists
-            if (this.to.indexOf(';') != -1) {
-                this.to = this.to.replaceAll(";", ",");
-            }
-            msg.setRecipients(Message.RecipientType.TO, this.to);  // 200102 LJM - 수정
-
-            if (this.cc.length() > 1) {
-                if (this.cc.indexOf(';') != -1) {
-                    this.cc = this.cc.replaceAll(";", ",");
-                }
-                msg.setRecipients(Message.RecipientType.CC, this.cc);
+            // 수신자/참조 주소 설정 (세미콜론을 쉼표로 변환)
+            msg.setRecipients(Message.RecipientType.TO, sanitizeAddress(this.to));
+            if (this.cc != null && this.cc.length() > 1) {
+                msg.setRecipients(Message.RecipientType.CC, sanitizeAddress(this.cc));
             }
 
-            //msg.setSubject(s);
-//            msg.setSubject(MimeUtility.encodeText(this.subj, "euc-kr", "B"));
-            msg.setSubject(this.subj);
-
-            //msg.setHeader("Content-Type", "text/plain; charset=utf-8");
+            msg.setSubject(this.subj);  // 제목 설정
             msg.setHeader("User-Agent", "LJM-WM/0.1");
-            //msg.setHeader("Content-Transfer-Encoding", "8bit");
-            //msg.setAllow8bitMIME(true);
 
-            // body
-            MimeBodyPart mbp = new MimeBodyPart();
-            // Content-Type, Content-Transfer-Encoding 설정 의미 없음.
-            // 자동으로 설정되는 것 같음. - LJM 041202
-            // mbp.setHeader("Content-Type", "text/plain; charset=euc-kr");
-            // mbp.setHeader("Content-Transfer-Encoding", "8bit");
-            mbp.setText(this.body);
+            // 본문 설정
+            MimeBodyPart bodyPart = new MimeBodyPart();
+            bodyPart.setText(this.body);
 
             Multipart mp = new MimeMultipart();
-            mp.addBodyPart(mbp);
+            mp.addBodyPart(bodyPart);
 
-            // 첨부 파일 추가
+            // 첨부파일이 있으면 추가
             if (this.file1 != null) {
-                MimeBodyPart a1 = new MimeBodyPart();
-                DataSource src = new FileDataSource(this.file1);
-                a1.setDataHandler(new DataHandler(src));
-                // 22011 LJM: 윈도우즈/우분투에 따라서 달라져야 함
-                int index = this.file1.lastIndexOf(File.separator);
-                String fileName = this.file1.substring(index + 1);
-                // "B": base64, "Q": quoted-printable
-                a1.setFileName(MimeUtility.encodeText(fileName, "UTF-8", "B"));
-                mp.addBodyPart(a1);
+                MimeBodyPart attachment = createAttachmentPart(this.file1);
+                mp.addBodyPart(attachment);
             }
-            msg.setContent(mp);
 
-            // 메일 전송
-            Transport.send(msg);
+            msg.setContent(mp);  // 본문 + 첨부 조합 설정
+            Transport.send(msg); // 메일 전송
 
-            // 메일 전송 완료되었으므로 서버에 저장된
-            // 첨부 파일 삭제함
-            if (this.file1 != null) {
-                File f = new File(this.file1);
-                if (!f.delete()) {
-                    log.error(this.file1 + ": 파일 삭제가 제대로 안 됨.");
-                }
-            }
-            status = true;
+            deleteUploadedFile();  // 업로드된 임시파일 삭제
+
+            return true;
         } catch (Exception ex) {
-            log.error("sendMessage() error: {}", ex);
-        } finally {
-            return status;
+            log.error("sendMessage() error: {}", ex.getMessage(), ex);
+            return false;
         }
-    }  // sendMessage()
+    }
+
+    /**
+     * 주소 문자열에서 세미콜론을 쉼표로 변환하여 여러 수신자 처리
+     */
+    private String sanitizeAddress(String address) {
+        return address.replace(";", ",");
+    }
+
+    /**
+     * 첨부파일을 MimeBodyPart 형태로 생성
+     */
+    private MimeBodyPart createAttachmentPart(String filepath) throws Exception {
+        MimeBodyPart attachment = new MimeBodyPart();
+        DataSource src = new FileDataSource(filepath);
+        attachment.setDataHandler(new DataHandler(src));
+
+        // 첨부파일 이름 인코딩
+        String fileName = new File(filepath).getName();
+        attachment.setFileName(MimeUtility.encodeText(fileName, "UTF-8", "B"));
+        return attachment;
+    }
+
+    /**
+     * 업로드된 첨부파일을 삭제
+     */
+    private void deleteUploadedFile() {
+        if (this.file1 == null) return;
+
+        try {
+            Files.deleteIfExists(new File(this.file1).toPath());
+            log.debug("File deleted: {}", this.file1);
+        } catch (IOException e) {
+            log.error("Failed to delete file {}: {}", this.file1, e.getMessage(), e);
+        }
+    }
 }
