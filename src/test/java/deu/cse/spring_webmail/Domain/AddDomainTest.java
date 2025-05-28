a84ffd04e472b49bf1a0fc5386f4af9cd6a50b39
minseong0202 <mskim5750@gmail.com>
2025-05-28 18:10:30 +0900

    도메인 추가 테스트코드 수정(도메인 추가하면 테스트후 삭제되도록 도메인 삭제 테스트 코드 추가)

--- src/test/java/deu/cse/spring_webmail/Domain/AddDomainTest.java
+++ src/test/java/deu/cse/spring_webmail/Domain/AddDomainTest.java
@@ -6,6 +6,8 @@ package deu.cse.spring_webmail.Domain;
 
 import deu.cse.spring_webmail.model.UserAdminAgent;
 import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,14 +21,21 @@ import org.springframework.web.client.RestTemplate;
 // 임시로 도메인 값을 넣고 james 서버에 추가되었는지 확인하는 테스트
 public class AddDomainTest {
     
+    // 값 준비
+    UserAdminAgent agent = new UserAdminAgent("localhost", 8000);
+    String testDomain = "test.com";
+    String url = "http://" + agent.getServer() + ":" + agent.getPort() + "/domains/" +  testDomain;
+    
+    // 테스트하기 전 테스트 값이 있다면 정리하기
+    @BeforeEach
+    void prepare() {
+        agent.deleteDomain(new String[]{testDomain});
+    }
+       
     @Test
     void test() {
-        // 값 준비
-        UserAdminAgent agent = new UserAdminAgent("localhost", 8000);
-        String testDomain = "test.com";
-        String url = "http://" + agent.getServer() + ":" + agent.getPort() + "/domains/" +  testDomain;
         
-        // 실행
+         // 실행
         String testAddDomain = agent.addDomain(testDomain);
         
         // 검증
@@ -35,4 +44,9 @@ public class AddDomainTest {
         assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
     }
     
+    // 테스트 후 테스트 값 정리하기
+    @AfterEach
+    void clean() {
+        agent.deleteDomain(new String[]{testDomain});
+    }
 }
